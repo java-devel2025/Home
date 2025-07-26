eb5651ac8a96609853119a23401cc1c1d0e67a43
java-devel2025 <kuznya9@mail.ru>
2025-07-26 16:49:12 +0700

    Homework 2

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,74 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        // Задача 1
+        var dog = 8.0;
+        var cat = 3.6;
+        var paper = 763789;
+
+        System.out.println(dog);
+        System.out.println(cat);
+        System.out.println(paper);
+
+        // Задача 2
+        dog = dog + 4;
+        System.out.println(dog);
+
+        cat = cat + 4;
+        System.out.println(cat);
+
+        paper = paper + 4;
+        System.out.println(paper);
+
+        // Задача 3
+        dog = dog - 3.5;
+        System.out.println(dog);
+
+        cat = cat - 1.6;
+        System.out.println(cat);
+
+        paper = paper - 7639;
+        System.out.println(paper);
+
+        // Задача 4
+        var friend = 19;
+        System.out.println(friend);
+
+        friend = friend + 2;
+        System.out.println(friend);
+
+        friend = friend / 7;
+        System.out.println(friend);
+
+        // Задача 5
+        var frog = 3.5;
+        System.out.println(frog);
+
+        frog = frog * 10;
+        System.out.println(frog);
+
+        frog = frog / 3.5;
+        System.out.println(frog);
+
+        frog = frog + 4;
+        System.out.println(frog);
+
+        // Задача 6
+        var boxer1 = 78.2;
+        var boxer2 = 82.7;
+        System.out.println(boxer1 + boxer2);
+        System.out.println(boxer2 - boxer1);
+
+        // Задача 7
+        System.out.println(boxer2 % boxer1);
+
+        // Задача 8
+        var allHour = 640;
+        var hour = 8;
+        var peoples = allHour / hour;
+        var newPeoples = 94;
+        System.out.println("Всего работников в компании — " + peoples + " человек");
+        System.out.println("Если в компании работает " + peoples + " человек, то всего " + allHour / newPeoples +
+                " часов работы может быть поделено между сотрудниками");
     }
 }
