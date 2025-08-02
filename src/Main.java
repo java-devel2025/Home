12d457595683e9e848e5ee4c5c0929dac37b8633
java-devel2025 <kuznya9@mail.ru>
2025-08-02 07:46:23 +0700

    не много сложновато на первых парах, но вроде понял.

--- src/Main.java
+++ src/Main.java
@@ -2,103 +2,83 @@ public class Main {
     public static void main(String[] args) {
 
         // Задача 1
-        byte age = 17;
+        byte clientOS = 0;
 
-        if (age >= 18){
-            System.out.println("Если возраст человека равен " + age + " он совершеннолетний");
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
         } else {
-            // Блок выполнится. Результат выражения в условии if был false
-            System.out.println("Если возраст человека равен " + age + " возраст совершеннолетия еще не наступил, нужно немного подождать.");
+            System.out.println("Установите версию приложения для Android по ссылке");
         }
 
-        // Задача 2
-        byte realTemperature = 17;
 
-        if (realTemperature < 5){
-            System.out.println("На улице холодно, нужно надеть шапку");
-        } else {
-            // Блок выполнится. Результат выражения в условии if был false
-            System.out.println("Сегодня тепло, можно идти без шапки");
+        // Задача 2
+        int clientDeviceYear = 2015;
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+            if (clientDeviceYear == 2015) {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+        }
+        if (clientOS == 1) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+          if (clientDeviceYear == 2015) {
+              System.out.println("Установите облегченную версию приложения для Android по ссылке");
+          }
         }
 
         // Задача 3
-        byte overSpeed = 17;
+        short year = 2021;
 
-        if (overSpeed > 60){
-            System.out.println("Скорость превышена");
+        if (year <= 1584) {
+            System.out.println(year + " год не проверяется, так как високосные годы ввели после 1584 года.");
+        } else if ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+            System.out.println(year + " год является високосным.");
         } else {
-            // Блок выполнится. Результат выражения в условии if был false
-            System.out.println("Превышения скорости нет");
+            System.out.println(year + " год не является високосным.");
         }
 
         // Задача 4
-        byte realAge = 24;
+        short deliveryDistance = 95;
+        byte days = 0;
 
-        if (realAge <= 6 && realAge >= 2){
-            System.out.println("Если возраст человека равен " + realAge + " тебе нужно ходить в детский сад");
-        }
-        if (realAge <= 17 && realAge >= 7){
-            System.out.println("Если возраст человека равен " + realAge + " тебе нужно ходить в школу");
-        }
-        if (realAge <= 24 && realAge >= 18){
-            System.out.println("Если возраст человека равен " + realAge + " тебе нужно ходить в университет");
-        }
-        if (realAge > 24) {
-            System.out.println("Если возраст человека равен " + realAge + ", то работать, трутень или начни учится в Skypro!");
+        if (deliveryDistance > 100) {
+            System.out.println("Доставка не осуществляется на расстояние более 100 км.");
+        } else {
+            if (deliveryDistance > 20 && deliveryDistance < 60) {
+                System.out.println(" Потребуется дней: " + (days + 1) + " срок доставки");
+            }
+            if (deliveryDistance > 60 && deliveryDistance < 100) {
+                System.out.println(" Потребуется дней: " + (days + 2) + " срок доставки");
+            }
         }
 
         // Задача 5
-        byte child = 14;
-        boolean adult = false;
+        byte monthNumber = 12;
 
-        if (child < 5) {
-            System.out.println("Если возраст ребенка равен " + child + " рано на аттракцион");
-        }
-        if (child < 14 && realAge >= 5 && !adult){
-            System.out.println("Если возраст ребенка равен " + child +  " и если взрослого рядом нет, то кататься нельзя");
-        }
-        if (child >= 14 && !adult){
-            System.out.println("Если возраст ребенка равен " + child + " он может кататься без сопровождения взрослого");
-        }
-
-
-        // Задача 6
-        byte trainCarriageAll = 102;
-        byte trainCarriageSeating = 60;
-        byte trainCarriageAvailableAll = 59;
-
-        if (trainCarriageAvailableAll < trainCarriageAll){
-            System.out.println("Есть место в вагоне");
-        } else {
-            System.out.println("Вагон уже полностью забит");
-        }
-        if (trainCarriageAvailableAll < trainCarriageSeating){
-            System.out.println("Есть сидячее место в вагоне");
-        }
-        if (trainCarriageAvailableAll > trainCarriageSeating && trainCarriageAvailableAll < trainCarriageAll) {
-            System.out.println("В вагоне остались только стоячие места");
-        }
-
-        // Задача 7
-        int one;
-        int two;
-        int three;
-
-        one = 1;
-        two = 2;
-        three = 3;
-
-        int truee = 0;
-
-        if (one > two) {
-            truee = one;
-        } else {
-            truee = two;
-        }
-        if (truee > three) {
-            System.out.println(truee = three);
-        } else {
-            System.out.println(truee = three);
+        switch (monthNumber) {
+            case 1:
+            case 2:
+            case 3:
+                System.out.println("Месяц " + monthNumber + " это зима");
+                break;
+            case 4:
+            case 5:
+            case 6:
+                System.out.println("Месяц " + monthNumber + " это лето");
+                break;
+            case 7:
+            case 8:
+            case 9:
+                System.out.println("Месяц " + monthNumber + " это осень");
+                break;
+            case 10:
+            case 11:
+            case 12:
+                System.out.println("Месяц " + monthNumber + " это зима");
+                break;
+            default:
+                break;
         }
     }
 }
